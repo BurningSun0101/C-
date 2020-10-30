e9188df07731e5eb80508b52617bbb839559372d
BurningSun0101 <73688927+BurningSun0101@users.noreply.github.com>
2020-10-30 17:49:02 +0800

    Rename ConsoleApplication7.cpp to 矩陣加法.cpp

---
+++ 矩陣加法.cpp
@@ -0,0 +1,41 @@
+﻿// ConsoleApplication7.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+const int rows = 2;
+const int columns = 3;
+
+void matrix_init(int a[][columns], int b[][columns])
+{
+	for (int i = 0; i < rows; i++)
+		for (int j = 0; j < columns; j++)
+			cin >> a[i][j];
+	for (int i = 0; i < rows; i++)
+		for (int j = 0; j < columns; j++)
+			cin >> b[i][j];
+}
+
+void matrix_add(int a[][columns], int b[][columns], int c[][columns])
+{
+	for (int i = 0; i < rows; i++)
+		for (int j = 0; j < columns; j++)
+			c[i][j] = a[i][j] + b[i][j];
+}
+
+void matrix_print(int arr[][columns])
+{
+	for (int i = 0; i < rows; i++)
+		for (int j = 0; j < columns; j++)
+			cout << arr[i][j];
+}
+
+int main(int argc, char *argv[])
+{
+	int A[rows][columns], B[rows][columns], C[rows][columns];
+	matrix_init(A, B);
+	matrix_add(A, B, C);
+	matrix_print(C);
+}
