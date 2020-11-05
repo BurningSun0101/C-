1fa95b223c641740226dd23fb38e7e9253fd0286
BurningSun0101 <73688927+BurningSun0101@users.noreply.github.com>
2020-11-05 12:29:01 +0800

    Update and rename ConsoleApplication8.cpp to 矩陣反轉.cpp

---
+++ 矩陣反轉.cpp
@@ -0,0 +1,29 @@
+﻿// ConsoleApplication8.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int Array_Data[5][5];
+	for (int i = 0; i < 5; i++)
+		for (int j = 0; j < 5; j++)
+			cin >> Array_Data[i][j];
+	for (int i = 0; i < 5; i++)
+	{
+		for (int z = 0;z < i;z++)
+		{
+			Array_Data[i][z] = 0;
+		}
+	}
+	for (int i = 0; i < 5; i++)
+		for (int j = 0; j < 5; j++)
+		{
+			if (Array_Data[i][j] != 0)
+				cout << Array_Data[i][j] << " ";
+
+		}
+
+}
+
