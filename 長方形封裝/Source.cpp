45f695916f008603d85d48532c26de33fd566d8f
BurningSun0101 <73688927+BurningSun0101@users.noreply.github.com>
2021-04-13 01:39:03 +0800

    Add files via upload

---
+++ 長方形封裝/Source.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<iomanip>
+#include"Rectangle.h"
+using namespace std;
+
+int main()
+{
+	double x, y, p, q;
+	cin >> x >> y >> p >> q;
+	Rectangle rectangle1(x, y);
+	Rectangle rectangle2(p, q);
+
+
+	cout << fixed << setprecision(2) << rectangle1.getArea() << " " << fixed << setprecision(2) << rectangle1.getPerimeter() << endl
+		<< fixed << setprecision(2) << rectangle2.getArea() << " " << fixed << setprecision(2) << rectangle2.getPerimeter() << endl;
+	rectangle2.setWidth(2.5);
+	rectangle2.setHeight(5.0);
+
+	cout << fixed << setprecision(2) << rectangle2.getArea() << " " << fixed << setprecision(2) << rectangle2.getPerimeter() << endl;
+
+
+
+	return 0;
+}
